12d9c6fcb4f79b56c1cdb7b3e18327fb6ccfafa1
fahadtahir111 <lalafahad407@gmail.com>
2026-04-25 02:27:24 +0500

    22132

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "../../"),
-  outputFileTracingExcludes: {
-    "*": [
-      "node_modules/@swc/core-linux-x64-gnu",
-      "node_modules/@swc/core-linux-x64-musl",
-      "node_modules/@esbuild/linux-x64",
-    ],
-  },
   images: {
     remotePatterns: [
       {
